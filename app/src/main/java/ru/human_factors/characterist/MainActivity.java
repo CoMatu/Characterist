c0c930c2516260d02df7ce3102363b6dfc09c09d
CoMatu <matusevichvv@gmail.com>
2018-03-29 12:38:15 +0500

    Прикрепил фрагмент в main_activity

--- app/src/main/java/ru/human_factors/characterist/MainActivity.java
+++ app/src/main/java/ru/human_factors/characterist/MainActivity.java
@@ -1,13 +1,31 @@
 package ru.human_factors.characterist;
 
+import android.app.ListFragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ru.human_factors.characterist.ItemFragment.OnListFragmentInteractionListener;
+import ru.human_factors.characterist.dummy.DummyContent;
 
+public class MainActivity extends AppCompatActivity implements OnListFragmentInteractionListener{
+    ItemFragment listFragment;
+    FragmentTransaction fTrans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listFragment = new ItemFragment();
+        fTrans = getSupportFragmentManager()
+                .beginTransaction();
+        fTrans.add(R.id.container, listFragment);
+        fTrans.commit();
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
